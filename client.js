f403c754e7413b1747ca679aa26df4b05d5afbda
Anisa Abdulkadir <anisa.ali7@gmail.com>
2017-03-10 21:15:49 -0600

    functions to add peopleArray, squares, buttons, counter. initial code.

--- client.js
+++ client.js
@@ -1,1 +1,54 @@
-console.log(peopleArray);
+var count = 0;
+var squareTabs = 20;
+var squareArray = [];
+
+$(document).ready(function() {
+// squareSelection();
+buttons();
+});
+
+
+// SQUARE TABS
+function squareSelection() {
+    for (var i = 0; i < squareTabs; i++) {
+        $(".container").append("<div class='tab'></div>");
+        var $el = $('.container').children().last();
+
+    }
+    // $('.tab').on("click", function() {
+    //   appendDom();
+  }
+
+
+//BUTTONS
+function buttons() {
+    $('.container').append('<div><button>Prev</button></div>',
+        '<div><button>Next</button></div>');
+    $('.button').on('click', 'button', function() {
+
+    });
+}
+
+//COUNTER
+function counter() {
+    $('.tab').on("click", function() {
+        console.log('count');
+        // count++;
+        // $(this).before().text('count' + count);
+    });
+}
+
+
+//PEOPLE ARRAY
+function appendDom() {
+    for (var i = 0; i < peopleArray.length; i++) {
+        $('.container').append('<div></div>');
+        var $el = $('.container').children().last();
+        $el.append('</p>' + peopleArray[i].name + '</p>');
+        $el.append('</p>' + peopleArray[i].shoutout + '</p>');
+    }
+}
+
+//HIGHLIGHT
+function highlight() {
+}
