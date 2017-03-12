4ad1ea120641a1c0fea8ddc99255344968920e3a
Anisa Abdulkadir <anisa.ali7@gmail.com>
2017-03-12 17:31:47 -0500

    button class change

--- client.js
+++ client.js
@@ -7,7 +7,10 @@ $(document).ready(function() {
     appendDom();
     buttons();
     buttonClick();
+    clickSquare();
+
 });
+
 
 function appendDom() {
     for (var i = 0; i < squares; i++) {
@@ -19,38 +22,37 @@ function appendDom() {
 
         $('.info').append('<div></div>');
         var $pl = $('.info').children().last();
-        $pl.append('<h3>' + peopleArray[i].name + '<h3>',
-                  '<h5>' + peopleArray[i].shoutout + '<h5>');
+        $pl.append('<p class="name">' + peopleArray[i].name + '<p>',
+                   '<p class="shout">' + peopleArray[i].shoutout + '<p>');
         $pl.data("person", i);
         shoutoutArray.push($pl);
-}
-highlightSquares();
+    }
+    highlightSquares();
 }
 
-//BUTTONS
+
 function buttons() {
-    $('.container').append('<div id="prev"><button>Prev</button></div>',
-        '<div id="next"><button>Next</button></div>');
+    $('.container').append('<div class="prev"><button>Prev</button></div>',
+        '<div class="next"><button>Next</button></div>');
 }
 
 function buttonClick() {
-    $('#next').on('click', function() {
-      count++;
-      if (count == squares) {
-        count = 0;
-      }
-      highlightSquares();
+    $('.next').on('click', function() {
+        count++;
+        if (count == squares) {
+            count = 0;
+        }
+        highlightSquares();
     });
-    $('#prev').on('click', function() {
-      count--;
-      if (count == squares) {
-        count = 0;
-      }
-      highlightSquares();
+    $('.prev').on('click', function() {
+        count--;
+        if (count == squares) {
+            count = 0;
+        }
+        highlightSquares();
     });
 }
 
-//HIGHLIGHT
 function highlightSquares() {
     for (var i = 0; i < shoutoutArray.length; i++) {
         if (squareArray[i].data("square") == count) {
@@ -61,24 +63,5 @@ function highlightSquares() {
             shoutoutArray[i].hide();
         }
     }
-    $('.page').text("Page Number: "+(count+1)+"/19");
+    $('.page').text("Chiyak: " + (count + 1) + "/19");
 }
-
-
-
-//HIGHLIGHT TIMING
-// function timingNext() {
-//     count++;
-//     if (count == squares) {
-//       count = 0;
-//     }
-//     highlightSquares();
-// }
-//
-// function timingPrev() {
-//     count--;
-//     if (count == squares) {
-//       count = 0;
-//     }
-//     highlightSquares();
-// }
