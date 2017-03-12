cbac81056781fbfa078e6e27d4b4bfb4461c2ff1
Anisa Abdulkadir <anisa.ali7@gmail.com>
2017-03-11 21:12:46 -0600

    peopleArray under info class

--- client.js
+++ client.js
@@ -5,12 +5,28 @@ var shoutoutArray = [];
 
 $(document).ready(function() {
     appendDom();
-
     buttons();
     buttonClick();
 
 });
 
+function appendDom() {
+    for (var i = 0; i < squares; i++) {
+        $(".container").append("<div class='square'></div>");
+        var $sq = $('.container').children().last();
+        $sq.data("square", i);
+        squareArray.push($sq);
+
+
+        $('.info').append('<div></div>');
+        var $pl = $('.info').children().last();
+        $pl.append('<h3>' + peopleArray[i].name + '<h3>',
+                  '<h5>' + peopleArray[i].shoutout + '<h5>');
+        $pl.data("person", i);
+        shoutoutArray.push($pl);
+}
+highlightSquares();
+}
 
 //BUTTONS
 function buttons() {
@@ -35,33 +51,15 @@ function buttonClick() {
     });
 }
 
-
-function appendDom() {
-    for (var i = 0; i < squares; i++) {
-        $(".container").append("<div class='square'></div>");
-        var $sq = $('.container').children().last();
-        $sq.data("square", i);
-        squareArray.push($sq);
-
-        $('.container').append('<div class="info"></div>');
-        var $pl = $('.container').children().last();
-        $pl.append('<h2>' + peopleArray[i].name + '<h2>',
-                  '<h4>' + peopleArray[i].shoutout + '<h4>');
-        $pl.data("person", i);
-        shoutoutArray.push($pl);
-}
-highlightSquares();
-}
-
 //HIGHLIGHT
 function highlightSquares() {
-    for (var i = 0; i < squareArray.length; i++) {
+    for (var i = 0; i < shoutoutArray.length; i++) {
         if (squareArray[i].data("square") == count) {
-            shoutoutArray[i].show();
             $(squareArray[i]).addClass("highlight");
+            shoutoutArray[i].show();
         } else if (squareArray[i].data("square") != count) {
-            shoutoutArray[i].hide();
             $(squareArray[i]).removeClass("highlight");
+            shoutoutArray[i].hide();
         }
     }
 }
